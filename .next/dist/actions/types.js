ca78739f4fdbb6e954ef93ee6187dfda48c1125d
vietkhoa <vietkhoa176@gmail.com>
2017-11-13 09:07:38 +0700

    delete .next

--- .next/dist/actions/types.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var FETCH_USERS = exports.FETCH_USERS = 'fetch_users';
-var FETCH_CURRENT_USER = exports.FETCH_CURRENT_USER = 'fetch_current_user';
-var FETCH_ADMINS = exports.FETCH_ADMINS = "fetch_admins";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFjdGlvbnMvdHlwZXMuanMiXSwibmFtZXMiOlsiRkVUQ0hfVVNFUlMiLCJGRVRDSF9DVVJSRU5UX1VTRVIiLCJGRVRDSF9BRE1JTlMiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sSUFBTSxvQ0FBYyxBQUFwQixBQUNQO0FBQU8sSUFBTSxrREFBcUIsQUFBM0IsQUFDUDtBQUFPLElBQU0sc0NBQWUsQUFBckIiLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiL2hvbWUva2hvYS85RnVyeS9SZWFjdC9uZXh0LXNlcnZlciJ9
